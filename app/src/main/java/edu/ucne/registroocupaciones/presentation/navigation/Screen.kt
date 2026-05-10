4272d7897d930c5950ce1d78d436dbf065c6f415
FelixAlbertoM <af301509@gmail.com>
2026-05-10 10:42:14 -0400

    Crea Screen.kt y DrawerItem.kt se encargan de la pantalla

---
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package edu.ucne.registroocupaciones.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed class Screen {
+
+    @Serializable
+    data object OcupacionList : Screen()
+
+    @Serializable
+    data class Ocupacion(val ocupacionId: Int) : Screen()
+}
