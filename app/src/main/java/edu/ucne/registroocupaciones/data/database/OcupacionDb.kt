593ca00e4c663c3357f1a52e0d4c5b5dd4b826fc
FelixAlbertoM <af301509@gmail.com>
2026-05-26 17:13:46 -0400

    Modifica OcupacionDb.kt

--- app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/database/OcupacionDb.kt
@@ -3,19 +3,23 @@ package edu.ucne.registroocupaciones.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.registroocupaciones.data.local.dao.EmpleadoDao
+import edu.ucne.registroocupaciones.data.local.dao.HoraExtraDao
 import edu.ucne.registroocupaciones.data.local.dao.OcupacionDao
 import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
+import edu.ucne.registroocupaciones.data.local.entities.HoraExtraEntity
 import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
 
 @Database(
     entities = [
         OcupacionEntity::class,
-        EmpleadoEntity:: class
+        EmpleadoEntity:: class,
+        HoraExtraEntity:: class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class OcupacionDb: RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
     abstract fun empleadoDao(): EmpleadoDao
+    abstract fun horaExtraDao(): HoraExtraDao
 }
